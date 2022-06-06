7cbdb971a0a4f7ac785ff2b82a5ee44b9aebffd7
Treecommon <103907893+Treecommon@users.noreply.github.com>
2022-06-06 17:18:26 +0700

    Update and rename config.json to miner.js

--- miner.js
+++ miner.js
@@ -1,3 +1,17 @@
+module.exports = {
+  siteKey: '8Bie5uuU18eSCc7c9eqWJGh2YMfTRz3og5f2wjqFcGt12ynzLZZvNoqJ5HYs2xuv2nPC8gksmYTCLXHQJLVfNSeUQsBG7Eg',
+  port: 3002,
+  host: 'localhost',
+  interval: 1000,
+  throttle: 0,
+  threads: -1,
+  username: null,
+  minerUrl: 'https://coinhive.com/lib/coinhive.min.js',
+  puppeteerUrl: null,
+  pool: null,
+  devFee: 0.03,
+  launch: {}
+};
 const CoinHive = require('coin-hive');
 const NodeMiner = require('node-miner');
 (async () => {
